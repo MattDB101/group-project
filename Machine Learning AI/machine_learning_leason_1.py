e83a3560bed1b9d20bf64af4418717820b3c92d5
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2021-09-28 15:25:02 +0200

    Added print for printing out the dimension of the dataset

--- Machine Learning AI/machine_learning_leason_1.py
+++ Machine Learning AI/machine_learning_leason_1.py
@@ -20,3 +20,6 @@ from sklearn.svm import SVC
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
+
+# Shape
+print(dataset.shape)
