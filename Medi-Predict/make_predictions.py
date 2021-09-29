ea8555435b1db7da33cd0c4536bc2800b331d5ac
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2021-09-29 12:39:18 +0200

    Added code for making predictions

--- Medi-Predict/make_predictions.py
+++ Medi-Predict/make_predictions.py
@@ -20,3 +20,18 @@ from sklearn.svm import SVC
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:, 0:4]
+y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
