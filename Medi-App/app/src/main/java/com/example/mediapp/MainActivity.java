d136b247590ade5e35d5cf44c4704081c7201c9c
felixlecat <liverpoolmax@hotmail.com>
2021-10-06 12:49:36 +0100

    Added new activity

--- Medi-App/app/src/main/java/com/example/mediapp/MainActivity.java
+++ Medi-App/app/src/main/java/com/example/mediapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mediapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signup_button = findViewById(R.id.button);
+        signup_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                System.out.println("Button Clicked");
+
+                Intent activity2Intent = new Intent(getApplicationContext(), login.class);
+                startActivity(activity2Intent);
+            }
+        });
     }
 }
