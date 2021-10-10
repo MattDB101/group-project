51ad06edaa343cb9da25d713153274b64bcafa68
Szymon Pawlica <szymon.pawlica@mycit.ie>
2021-10-10 02:13:20 +0100

    Changes to main

--- Medi-Coin/BlockChain/Main1.java
+++ Medi-Coin/BlockChain/Main1.java
@@ -1,10 +1,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    Blockchain blockchain = new Blockchain(4);
-    blockchain.addBlock(blockchain.newBlock("Tout sur le Bitcoin"));
-    blockchain.addBlock(blockchain.newBlock("Sylvain Saurel"));
-    blockchain.addBlock(blockchain.newBlock("https://www.toutsurlebitcoin.fr"));
+    Blockchain blockchain = new Blockchain();
 
     System.out.println("Blockchain valid ? " + blockchain.isBlockChainValid());
     System.out.println(blockchain);
