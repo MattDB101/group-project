03d151d37b50fcedb6a1937694b33175d2077788
Naphatsakorn Khotsombat <n.khotsombat@mycit.ie>
2021-10-13 12:02:59 +0200

    Refactor the code to add classification report

--- Medi-Predict/Heart-Disease-Prediction/heart-prediction.py
+++ Medi-Predict/Heart-Disease-Prediction/heart-prediction.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler as ss
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 df = pd.read_csv('extras/cleveland.csv', header=None)
 
@@ -20,16 +21,15 @@ df['thal'] = df.thal.fillna(df.thal.mean())
 df['ca'] = df.ca.fillna(df.ca.mean())
 
 # distribution of target vs age
-sns.set_context("paper", font_scale=2, rc={"font.size": 20, "axes.titlesize": 25, "axes.labelsize": 20})
-sns.catplot(kind='count', data=df, x='age', hue='target', order=df['age'].sort_values().unique())
-plt.title('Variation of Age for each target class')
-plt.show()
+# sns.set_context("paper", font_scale=2, rc={"font.size": 20, "axes.titlesize": 25, "axes.labelsize": 20})
+# sns.catplot(kind='count', data=df, x='age', hue='target', order=df['age'].sort_values().unique())
+# plt.title('Variation of Age for each target class')
+# plt.show()
 
 # barplot of age vs sex with hue = target
-sns.catplot(kind='bar', data=df, y='age', x='sex', hue='target')
-plt.title('Distribution of age vs sex with the target class')
-plt.show()
-
+# sns.catplot(kind='bar', data=df, y='age', x='sex', hue='target')
+# plt.title('Distribution of age vs sex with the target class')
+# plt.show()
 
 df['sex'] = df.sex.map({'female': 0, 'male': 1})
 
@@ -37,24 +37,26 @@ df['sex'] = df.sex.map({'female': 0, 'male': 1})
 X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_validation, y_train, Y_validation = train_test_split(X, y, test_size=0.2, random_state=0)
 
 sc = ss()
 X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+X_validation = sc.transform(X_validation)
 
 # SVM
-classifier = SVC(kernel='rbf')
-classifier.fit(X_train, y_train)
+model = SVC(kernel='rbf')
+model.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+y_pred = model.predict(X_validation)
 
-cm_test = confusion_matrix(y_pred, y_test)
+cm_test = confusion_matrix(y_pred, Y_validation)
 
-y_pred_train = classifier.predict(X_train)
+y_pred_train = model.predict(X_train)
 cm_train = confusion_matrix(y_pred_train, y_train)
+predictions = model.predict(X_validation)
 
 print()
 print('Accuracy for training set for svm = {}'.format((cm_train[0][0] + cm_train[1][1]) / len(y_train)))
-print('Accuracy for test set for svm = {}'.format((cm_test[0][0] + cm_test[1][1]) / len(y_test)))
+print('Accuracy for test set for svm = {}'.format((cm_test[0][0] + cm_test[1][1]) / len(Y_validation)))
+print('\n' + classification_report(Y_validation, predictions))
