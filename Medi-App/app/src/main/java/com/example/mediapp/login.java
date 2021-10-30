9b2172be1a285e5ab9d7014287c9d160d8c8e43e
felixlecat <liverpoolmax@hotmail.com>
2021-10-30 23:38:51 +0100

    Added sign in and email verification code

--- Medi-App/app/src/main/java/com/example/mediapp/login.java
+++ Medi-App/app/src/main/java/com/example/mediapp/login.java
@@ -1,15 +1,23 @@
 package com.example.mediapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
 
@@ -17,11 +25,14 @@ public class login extends AppCompatActivity {
     Button back_button, login_button;
     TextView info_text;
     int counter = 3;
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         back_button = findViewById(R.id.button4);
         back_button.setOnClickListener(new View.OnClickListener(){
@@ -42,7 +53,7 @@ public class login extends AppCompatActivity {
             public void onClick(View v){
                 System.out.println("Login Button Clicked");
 
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                /*if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     Toast.makeText(getApplicationContext(), "Welcome, admin.", Toast.LENGTH_SHORT).show();
                 }
                 else{
@@ -57,10 +68,74 @@ public class login extends AppCompatActivity {
                         login_button.setEnabled(false);
                         System.out.println("Too many incorrect login, please check login details");
                     }
-                }
+                }*/
             }
         });
 
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationEmail(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "Verification Email sent");
+                    }
+                }
+            });
+        }
+        mAuth.setLanguageCode("en");
+    }
+
+    private void sendPasswordReset(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String email = user.getEmail();
+            mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "Password Reset Email sent");
+                    }
+                }
+            });
+        }
+    }
+
+    private void reload(){ }
+
+    private void updateUI(FirebaseUser user){ }
 
 }
