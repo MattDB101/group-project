fc04abac7fc288f3ffb998c43162a354ec5101ae
Matthew Byrne <matt@invi.so>
2021-11-24 14:51:04 +0000

    fix add patient

--- Medi-Web/public/scripts/panel.js
+++ Medi-Web/public/scripts/panel.js
@@ -20,7 +20,7 @@ nameSpan = document.createElement("span")
 roleSpan = document.createElement("span")
 phoneSpan = document.createElement("span")
 addressSpan = document.createElement("p")
-
+const patList = document.querySelector('#pat-list');
 
 
 firebase.auth().onAuthStateChanged(user => {
@@ -60,6 +60,47 @@ function update(user) {
         document.getElementById("phone").appendChild(phoneSpan)
         document.getElementById("address").appendChild(addressSpan)
     }).catch((error) => window.location.replace("/details"));
+
+    db.collection('doctors').doc(user.email).collection('patients').get().then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            let li = document.createElement('li');
+            let name = document.createElement('span');
+            let email = document.createElement('span');
+            email.style.display = "block"
+            let tel = document.createElement('span');
+            tel.style.display = "block"
+            let add = document.createElement('span');
+            add.style.display = "block"
+
+            li.setAttribute('data-id', doc.id);
+
+            add.textContent = doc.data().add2 + " " + doc.data().add1 + " " + doc.data().city
+            name.textContent = doc.data().fname + " " + doc.data().lname
+            email.textContent = doc.data().email;
+            tel.textContent = doc.data().tel;
+
+            li.appendChild(name);
+            li.appendChild(email);
+            li.appendChild(tel);
+            li.appendChild(add);
+            newlink = document.createElement('a');
+            newlink.innerHTML = 'Delete';
+            newlink.setAttribute('title', 'Delete');
+            newlink.setAttribute('href', 'javascript:void(0)');
+            li.appendChild(newlink);
+
+            patList.appendChild(li);
+
+            // deleteing data
+            newlink.addEventListener("click", (e) => {
+                e.stopPropagation()
+                let id = e.target.parentElement.getAttribute("data-id")
+                console.log(id)
+                db.collection('doctors').doc(user.email).collection('patients').doc(id).delete();
+                location.reload()
+            })
+        });
+    });
 }
 
 
@@ -72,29 +113,44 @@ document.getElementById("addPatient").addEventListener("submit", (event) => {
         currentUser
     } = firebase.auth();
 
-    db.collection("patient").where("email", "==", patEmail).get().then(snapshot => {
-        const doc = snapshot.docs[0];
-        patFname = doc.data().fname;
-        patLName = doc.data().lname;
-        patTelephone = doc.data().tel;
-        patAddress1 = doc.data().add1;
-        patAddress2 = doc.data().add2;
-        patCity = doc.data().city;
-        patRegion = doc.data().region;
-        patZip = doc.data().zip;
-        patCountry = doc.data().country;
-    }).then(() => {
-        (db.collection('doctors').doc(currentUser.email).collection('patients').add({
-            fname: patFname,
-            lname: patLName,
-            tel: patTelephone,
-            add1: patAddress1,
-            add2: patAddress2,
-            city: patCity,
-            region: patRegion,
-            zip: patZip,
-            country: patCountry
+    db.collection('doctors').doc(currentUser.email).collection('patients').where("email", "==", patEmail).get().then(snapshot => {
+        snapshot.docs.forEach(doc => {
+        doc.ref.delete()
+        errMsg.innerHTML = "You already own this patient";
+        errMsg.style.display = "block";
         })
-        )}
-    )
+    }).then(() => {
+
+        db.collection("patient").where("email", "==", patEmail).get().then(snapshot => {
+            const doc = snapshot.docs[0];
+            patFname = doc.data().fname;
+            patLName = doc.data().lname;
+            patTelephone = doc.data().tel;
+            patAddress1 = doc.data().add1;
+            patAddress2 = doc.data().add2;
+            patCity = doc.data().city;
+            patRegion = doc.data().region;
+            patZip = doc.data().zip;
+            patCountry = doc.data().country;
+        }).then(() => {
+            (db.collection('doctors').doc(currentUser.email).collection('patients').add({
+                fname: patFname,
+                lname: patLName,
+                email: patEmail,
+                tel: patTelephone,
+                add1: patAddress1,
+                add2: patAddress2,
+                city: patCity,
+                region: patRegion,
+                zip: patZip,
+                country: patCountry
+            }))
+            location.reload()
+        }).catch(err => {
+            var errMsg = document.getElementById("error");
+            errMsg.innerHTML = "Email not linked to a user.";
+            errMsg.style.display = "block";
+            console.log("Wrong email!")
+        })
+    })
 });
